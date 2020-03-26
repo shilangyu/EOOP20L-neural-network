7f059c8ca51628b6eaa741654df841bad39a5c54
shilangyu <xmarcinmarcin@gmail.com>
2020-03-26 22:36:36 +0100

    testing for header correctness

--- tests/main.cpp
+++ tests/main.cpp
@@ -3,6 +3,13 @@
 
 using namespace std;
 
+// including all headers is a test for their correctness
+#include "NN/config.hpp"
+#include "NN/functions.hpp"
+#include "NN/matrix.hpp"
+#include "NN/neural_network.hpp"
+#include "NN/serialize.hpp"
+
 int main() {
   assert(1 == 1 && "Testing test");
 

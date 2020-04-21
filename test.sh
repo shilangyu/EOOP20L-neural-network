c65acbfaa0d821a81d1f50c983d609dc021f561d
shilangyu <xmarcinmarcin@gmail.com>
2020-04-21 11:13:28 +0200

    added UB sanitizer with libubsan

--- test.sh
+++ test.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-find test/ -name '*.cpp' | xargs g++ -Wall -Wextra -Werror -pedantic -std=c++2a -I"./include" -o tests \
+find test/ -name '*.cpp' | xargs g++ -Wall -Wextra -Werror -pedantic -fsanitize=undefined -std=c++2a -I"./include" -o tests \
 	&& ./tests \
 	&& rm ./tests
